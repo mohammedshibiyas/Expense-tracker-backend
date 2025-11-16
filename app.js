316caabb1acb72a0325e234e239440c61ca04b23
shibiyas <shibiyasshibi7@gmail.com>
2025-11-16 21:54:07 +0530

    add

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 // };
 
 app.use(cors({
-  origin: 'https://expense-tracker-b29b5.web.app',
+  origin: '*',
   methods: ['GET','POST','PUT','DELETE','PATCH','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization'],
   credentials: true
