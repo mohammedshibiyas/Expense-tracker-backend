120559dfea46850f0c947bc21de1a76f27f4c0e9
shibiyas <shibiyasshibi7@gmail.com>
2025-11-16 20:51:12 +0530

    add

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ const auth = require('./middleware/auth');
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://expense-tracker-b29b5.web.app',
+  methods: ['GET','POST','PUT','DELETE','PATCH','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization']
+}));
 
 // ----------------------
 // MongoDB Connection
